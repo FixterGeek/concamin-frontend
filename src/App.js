f4886f690395ed9a0e22c6be92e4bb669ede0717
Oswaldo Martinez <oswaldinho@MacBook-Pro-de-Oswaldo.local>
2018-05-19 18:59:33 -0500

    added routes

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {Button} from '@material-ui/core/';
+import Routes from './Routes';
 
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Button variant="raised" color="primary">
-      Hello World
-    </Button>
+        <Routes/>
       </div>
     );
   }
