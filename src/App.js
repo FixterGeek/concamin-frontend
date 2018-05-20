1477595d0d88a2686e31c53029a7a0265d126caf
Brenda Gonzalez <bremin11.20.93@gmail.com>
2018-05-19 20:25:36 -0500

    perfil

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import {Button} from '@material-ui/core/';
 import Routes from './Routes';
 
 
--- src/componentes/profile/ProfileDisplay.js
+++ src/componentes/profile/ProfileDisplay.js
@@ -1,20 +1,36 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Profile.css';
 import Paper from '@material-ui/core/Paper';
 import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
 
 export const ProfileDisplay = () => {
     return (
         <div className='padding-gral'>
-            <Paper  elevation={4}>
-                <div className='profile-image'>
+            <Paper  elevation={4} style={{minHeight: "379px"}}>
+                <div className='profile-img'>
 
                 </div>
-                <Avatar
-                    alt="User"
-                    src="https://scontent.fmex5-1.fna.fbcdn.net/v/t1.0-9/32893588_10216503871007215_7637282803905724416_n.jpg?_nc_cat=0&oh=7867ee410eda379ae94e4018ed4a42ba&oe=5B8F3876"
-                    style={{width:'60px', height:'60px'}}
-                />
+                <div style={{display:"flex", flexWrap:"wrap", justifyContent:"space-between", alignItems:"center"}}>
+                <div style={{display:"flex", flexWrap:"wrap"}}>
+                    <Avatar
+                        alt="User"
+                        src="https://scontent.fmex5-1.fna.fbcdn.net/v/t1.0-9/32893588_10216503871007215_7637282803905724416_n.jpg?_nc_cat=0&oh=7867ee410eda379ae94e4018ed4a42ba&oe=5B8F3876"
+                        style={{width:'150px', height:'150px', position:"relative", top:"-60px", marginLeft:"75px", border:"3px solid white" }}
+                    />
+                    <div className="data-user">
+                        <h4 className="title ">Brenda Gonzalez Ortega</h4>
+                        <p>Puesto</p>
+                        <p>en <strong>x organizacion</strong></p>
+                        <br/>
+                        <p><strong className="number">1000</strong> followers</p>
+                    </div>
+                </div>
+                    <Button variant="outlined" color="primary" style={{height:"50px", margin:"0 20px"}} >
+                        Editar perfil
+                    </Button>
+
+                </div>
             </Paper>
         </div>
     )
