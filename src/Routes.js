9df8d75087a0c9a9b100641b95090cec72072f01
Foggyrocket <dyl.blink.1.8.2@gmail.com>
2018-05-19 21:01:04 -0500

    Carddisplay

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
-import { PostCard } from './components/newsfeed/PostCard';
-
+import { PostCard} from './components/newsfeed/PostCard';
+import { CardDisplay} from './components/newsfeed/CardDisplay';
 
 const Routes =()=>(
     <Switch>
+        <Route path="/card" component={CardDisplay}/>
         <Route path="/" component={PostCard}/>
+
     </Switch>
 );
 
