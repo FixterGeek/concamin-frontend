3ef62d90ae99f2da27e48cc37696dea79f560ce9
Oswaldo Martinez <oswaldinho@MacBook-Pro-de-Oswaldo.local>
2018-05-19 19:47:23 -0500

    sgv icons

--- src/Routes.js
+++ src/Routes.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
+import { PostCard } from './components/newsfeed/PostCard';
 
 
 const Routes =()=>(
     <Switch>
-        
+        <Route path="/" component={PostCard}/>
     </Switch>
 );
 
