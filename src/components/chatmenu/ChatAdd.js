c7cc7d4faaf939502e5278b9c0e18832a96e31a9
MefitHp <mefitDev@gmail.com>
2018-05-21 18:05:09 -0500

    Chat to endpoint

--- src/components/chatmenu/ChatAdd.js
+++ src/components/chatmenu/ChatAdd.js
@@ -5,35 +5,76 @@ import Input from '@material-ui/core/Input';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import Typography from '@material-ui/core/Typography';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemText from '@material-ui/core/ListItemText'
 import Avatar from '@material-ui/core/Avatar'
 import ImageIcon from '@material-ui/icons/Image'
+import Button from '@material-ui/core/Button'
+import { Create, Close, ExpandMore } from '@material-ui/icons/';
 
 
-export const ChatAdd = ({ listi, textInput, onChange, close, onClose, onChangeHandler, pushButton, visible, expanded }) => {
+export const ChatAdd = ({ onClickChat, handleInput, userSelected, listi, textInput, onChange, close, onClose, onChangeHandler, pushButton, visible, expanded }) => {
+
 
     let list = listi
         .filter(a => textInput.toLowerCase() === '' || a.name.toLowerCase().includes(textInput.toLowerCase()))
-        .map((a, index) => <ListItem button key={index} onClick={() => onChange(a)}>
+        .map((a, index) => <ListItem button key={index} onChange={() => onChange(a)} onClick={() => onClickChat(a)}>
             <Avatar>
                 <ImageIcon />
             </Avatar>
             <ListItemText primary={a.name} />
         </ListItem>);
 
+    let msgList = [
+        { id: 1, msg: "mensaje 1", user: "Mefit" },
+        { id: 2, msg: "mensaje 2", user: "Mefit" },
+        { id: 3, msg: "mensaje 2", user: "Mefit" },
+    ]
 
+    if (userSelected) return (
+        <div>
+            <ExpansionPanel style={{
+                minWidth: '320px',
+                position: 'fixed',
+                bottom: 0,
+                right: 350,
+            }} defaultExpanded={true} hidden={close}>
+                <ExpansionPanelSummary expanded={true} expandIcon={<Close onClick={onClose} />} style={{ backgroundColor: 'dimgray' }}>
+                    <Typography>{userSelected}</Typography>
+                </ExpansionPanelSummary>
+                <ExpansionPanelDetails style={{ padding: 0 }} >
+                    <div style={{
+                        width: '100%',
+                    }}>
+                        <section style={{ height: '60vh' }}>
+                            <ul>
+                                {msgList.map((message, index) => {
+                                    return <li key={index}>{message.user}: {message.msg} </li>
+                                })}
+                            </ul>
+                        </section>
+                        <div style={{ width: '100%', backgroundColor: 'dimgray', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                            <Input style={{ height: 50, flexGrow: 2, paddingLeft: 10 }} name="messageInput" onChange={handleInput}
+                                placeholder="Escribe un mensaje.."
+                            />
+                            <Button style={{ marginRight: 5 }} variant="fab" mini color="secondary" aria-label="Create" onClick={this.onAdd}>
+                                <Create />
+                            </Button>
+                        </div>
+                    </div>
+                </ExpansionPanelDetails>
+            </ExpansionPanel>
+        </div >
+    )
     return (
-
         <ExpansionPanel style={{
             minWidth: '320px',
             position: 'fixed',
             bottom: 0,
-            right: 500,
+            right: 350,
         }} defaultExpanded={expanded} hidden={close}>
-            <ExpansionPanelSummary expandIcon={<ExpandMoreIcon onClick={onClose} />} style={{ backgroundColor: 'dimgray' }}>
+            <ExpansionPanelSummary expandIcon={<ExpandMore />} style={{ backgroundColor: 'dimgray' }}>
                 <Typography>Nuevo mensaje..</Typography>
             </ExpansionPanelSummary>
 
@@ -46,14 +87,10 @@ export const ChatAdd = ({ listi, textInput, onChange, close, onClose, onChangeHa
                             value={textInput}
                             onChange={onChangeHandler}
                         />
-
                     </div>
                     <section style={{ height: '60vh' }}>
                         <List>{list}</List>
                     </section>
-
-                    <button onClick={pushButton} hidden={visible} >perro</button>
-
                 </div>
             </ExpansionPanelDetails>
         </ExpansionPanel>
