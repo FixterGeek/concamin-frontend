5cc893b7dc71602417a89f90adf1cb397726a8ac
MefitHp <mefitDev@gmail.com>
2018-06-20 11:01:02 -0500

    missing messenger route

--- src/Routes.js
+++ src/Routes.js
@@ -11,7 +11,6 @@ const Routes = () => (
         <Route path="/login" component={LoginPage} />
         <Route exact path="/" component={HomeContainer} />
         <Route path="/main" component={MainPage} />
-        <Route path="/messenger" component={MessengerMain} />
     </Switch>
 );
 
